35f694a1d674d194a3bf2db5954f9888ee3218f3
Jeff <Jeff.t.nobl@gmail.com>
2020-01-08 14:43:04 -0700

    commit is acting funny

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs')
 const {
     check,
     validationResult
 } = require('express-validator');
+
+
+// importing the user model
+const User = require('../../models/User')
 
 // @route  post api/users
 // @desc   Register Route
@@ -16,12 +22,52 @@ router.post('/', [
         .isEmail(),
     check('password', 'Please enter a password with 8 or more characters')
         .isLength({ min: 8})
-], (req, res) => {
+],
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
-    res.send('User Route')
+
+    // check if user exists
+    const { username, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email});
+
+        if (user) {
+            res.status(400).json({ errors: [{ msg: 'User already exists'}] });
+        }
+        // Get users Gravatar
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        user = new User({
+            username,
+            email,
+            avatar,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(13);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+         // Return jsonwebtoken
+        res.send('User Registered')
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+
+    // Return jsonwebtoken
+
+
     });
 
 module.exports = router;
